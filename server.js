bba47a483187b6af33c0d6ed6cd896ebdced9f25
Xingxin Zeng <star_yes@qq.com>
2018-03-08 17:58:16 +0800

    File can be uploaded

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ app.use(fileUpload());
 app.post('/upload', function(req, res) {
   if (!req.files) return res.status(400).send('No files were uploaded.');
 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  let sampleFile = req.files.sampleFile;
+  let filepond = req.files.filepond;
 
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv('/somewhere/on/your/server/filename.jpg', function(err) {
+  // TODO: create files folder if it is not exist
+  filepond.mv(`./files/${filepond.name}`, function(err) {
     if (err) return res.status(500).send(err);
 
     res.send('File uploaded!');
