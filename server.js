9eb88820b5151e1cadcf5c52cb41a9fbc211001c
Xingxin Zeng <star_yes@qq.com>
2018-03-09 11:19:41 +0800

    Uploaded file can be reverted

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.text({ type: 'text/plain' }));
 
 // default options
 app.use(fileUpload());
 
+function getUniqueFilename(filename) {
+  return filename;
+}
+
+function getFilePath(filename) {
+  return `./files/${filename}`;
+}
+
 app.post('/upload', function(req, res) {
   if (!req.files) return res.status(400).send('No files were uploaded.');
 
-  let filepond = req.files.filepond;
+  const filepond = req.files.filepond;
 
   // TODO: create files folder if it is not exist
-  filepond.mv(`./files/${filepond.name}`, function(err) {
+  const filename = getUniqueFilename(filepond.name);
+  filepond.mv(getFilePath(filename), function(err) {
     if (err) return res.status(500).send(err);
 
-    res.send('File uploaded!');
+    res.send(filename);
+  });
+});
+
+app.delete('/upload', function(req, res) {
+  const filename = req.body;
+
+  fs.unlink(getFilePath(filename), function(err) {
+    if (err) return res.status(500).send(err);
+
+    res.send('success!');
   });
 });
 
