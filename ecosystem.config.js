69d1e3c7e4366416e4ba4a434ce34f8a53efa631
Xingxin Zeng <star_yes@qq.com>
2018-03-11 22:04:05 +0800

    Add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    {
+      name: 'quick-file',
+      script: './server.js',
+      env: {
+        COMMON_VARIABLE: 'true',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: 'ec2-user',
+      host: 'ec2-13-229-88-236.ap-southeast-1.compute.amazonaws.com',
+      key: '~/.ssh/AWS.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:SBeator/quick-file.git',
+      path: '/var/www/production',
+      'post-deploy':
+        'yarn install && pm2 reload ecosystem.config.js --env production',
+    },
+    'local-deploy': {
+      user: 'ec2-user',
+      host: 'localhost',
+      ref: 'origin/master',
+      repo: 'git@github.com:SBeator/quick-file.git',
+      path: '/var/www/production',
+      'post-deploy':
+        'yarn install && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
