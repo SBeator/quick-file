36d5e26ddfde0f4752c07b78f3f1f47987e70734
Xingxin Zeng <star_yes@qq.com>
2018-03-11 21:52:26 +0800

    Add login feature and move the root path to /file

---
+++ file.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var router = express.Router();
+
+const fs = require('fs');
+const path = require('path');
+
+const FILE_ROOT_PATH = './files/';
+
+function getUniqueFilename(filename) {
+  return filename;
+}
+
+function getFilePath(filename) {
+  return `./files/${filename}`;
+}
+
+function isAuth(req) {
+  return req.session.auth;
+}
+
+function setAuth(req, auth) {
+  req.session.auth = true;
+}
+
+function loginSuccess(req) {
+  return req.body.password === 'xingxin';
+}
+
+router.get('/', function(req, res, next) {
+  if (isAuth(req)) {
+    next();
+  } else {
+    res.redirect(req.originalUrl + 'login');
+  }
+});
+
+router.get('/login', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/login.html'));
+});
+
+router.post('/login', function(req, res) {
+  if (loginSuccess(req)) {
+    setAuth(req, true);
+    res.redirect(req.originalUrl.replace('/login', ''));
+  } else {
+    console.log(req);
+    res.redirect(req.originalUrl);
+  }
+});
+
+router.use(function(req, res, next) {
+  if (isAuth(req)) {
+    next();
+  } else {
+    res.status(401).send('');
+  }
+});
+
+router.get('/filelist', function(req, res) {
+  fs.readdir(FILE_ROOT_PATH, function(err, items) {
+    if (err) return res.status(500).send(err);
+
+    res.send(
+      items.filter(item => !/(^|\/)\.[^\/\.]/g.test(item)).map(item => ({
+        filename: item,
+      }))
+    );
+  });
+});
+
+router.get('/download/:filename', function(req, res) {
+  res.download(getFilePath(req.params.filename));
+});
+
+router.post('/upload', function(req, res) {
+  if (!req.files) return res.status(400).send('No files were uploaded.');
+
+  const filepond = req.files.filepond;
+
+  // TODO: create files folder if it is not exist
+  const filename = getUniqueFilename(filepond.name);
+  filepond.mv(getFilePath(filename), function(err) {
+    if (err) return res.status(500).send(err);
+
+    res.send(filename);
+  });
+});
+
+router.delete('/upload', function(req, res) {
+  const filename = req.body;
+
+  fs.unlink(getFilePath(filename), function(err) {
+    if (err) return res.status(500).send(err);
+
+    res.send('success!');
+  });
+});
+
+router.use(express.static('public'));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,66 +1,25 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const fs = require('fs');
+const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 
+const fileRouter = require('./file');
+
 const PORT = 3000;
-const FILE_ROOT_PATH = './files/';
 
 const app = express();
 
 app.use(bodyParser.text({ type: 'text/plain' }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// default options
 app.use(fileUpload());
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['auth'],
+  })
+);
 
-function getUniqueFilename(filename) {
-  return filename;
-}
-
-function getFilePath(filename) {
-  return `./files/${filename}`;
-}
-
-app.get('/filelist', function(req, res) {
-  fs.readdir(FILE_ROOT_PATH, function(err, items) {
-    if (err) return res.status(500).send(err);
-
-    res.send(
-      items.filter(item => !/(^|\/)\.[^\/\.]/g.test(item)).map(item => ({
-        filename: item,
-      }))
-    );
-  });
-});
-
-app.get('/download/:filename', function(req, res) {
-  res.download(getFilePath(req.params.filename));
-});
-
-app.post('/upload', function(req, res) {
-  if (!req.files) return res.status(400).send('No files were uploaded.');
-
-  const filepond = req.files.filepond;
-
-  // TODO: create files folder if it is not exist
-  const filename = getUniqueFilename(filepond.name);
-  filepond.mv(getFilePath(filename), function(err) {
-    if (err) return res.status(500).send(err);
-
-    res.send(filename);
-  });
-});
-
-app.delete('/upload', function(req, res) {
-  const filename = req.body;
-
-  fs.unlink(getFilePath(filename), function(err) {
-    if (err) return res.status(500).send(err);
-
-    res.send('success!');
-  });
-});
-
-app.use(express.static('public'));
+app.use('/file', fileRouter);
 
 app.listen(PORT, () => console.log(`Server host on http://localhost:${PORT}!`));
