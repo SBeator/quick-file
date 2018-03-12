f81e49ac87478539c41d336b347acf2153c13f90
Xingxin Zeng <star_yes@qq.com>
2018-03-12 09:52:26 +0800

    Remove production deploy config to hidden aws infomation

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,16 +21,6 @@ module.exports = {
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
   deploy: {
-    production: {
-      user: 'ec2-user',
-      host: 'ec2-13-229-88-236.ap-southeast-1.compute.amazonaws.com',
-      key: '~/.ssh/AWS.pem',
-      ref: 'origin/master',
-      repo: 'git@github.com:SBeator/quick-file.git',
-      path: '/var/www/quick-file',
-      'post-deploy':
-        'yarn install && pm2 reload ecosystem.config.js --env production',
-    },
     'local-deploy': {
       user: 'ec2-user',
       host: 'localhost',
